417d1b66a56b1432cc1b55f12e268e24238b4177
Trần Ngọc Huy <truongduchuy910@gmail.com>
2019-03-31 14:04:37 +0700

    add

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//nodemon --exec "heroku local" --signal SIGTERM
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -6,5 +8,13 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/', (req, res) => res.render('pages/index.ejs'))
+  .get('/camket', (req, res) => res.render('pages/camket.ejs', {
+    company: {
+      name: "Yến Sào Đất Quảng",
+      logo: "/images/logos/Logo-white.png",
+    },
+    product: []
+
+  }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+function modify_intro() {
+    var page = document.getElementById('page');
+    var intro = document.getElementById('intro');
+    var banner_slide = document.getElementById('banner-slide');
+    if (banner_slide.clientHeight > Number(intro.clientHeight) / 2) {
+        page.style.top = "-" + String(Number(intro.clientHeight) / 2) + "px";
+    } else {
+        page.style.top = "-" + String(Number(banner_slide.clientHeight) / 2) + "px";
+    }
+}
+modify_intro();
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+        if (pageYOffset > 100) {
+            document.documentElement.scrollTop = 0;
+        }
+        
+    })
+})
