b259938fdddeb3ae64b3be78330dcdab1b9c0805
truongduchuy <truongduchuy910@gmail.com>
2020-03-18 00:19:21 +0000

    OK

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 
 module.exports = function () {
     console.log(process.env.uri)
-    mongoose.connect("mongodb://localhost:27017/yensaodatquang?retryWrites=true", { useNewUrlParser: true }, (err) => {
+    mongoose.connect("mongodb://yensaodatquang:Yensaodatquang.vn@139.180.214.47:27017/yensaodatquang?retryWrites=true", { useNewUrlParser: true }, (err) => {
         if (!err) {
             console.log('Kết nối thành công tới mongodb');
         } else {
